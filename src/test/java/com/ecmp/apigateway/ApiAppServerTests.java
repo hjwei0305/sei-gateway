8d078250f842727b6c80140871caa55c4502f194
贺俊 <jun5.he@changhong.com>
2018-04-26 15:13:28 +0800

    修改获取配置中心的接口

--- src/test/java/com/ecmp/apigateway/ApiAppServerTests.java
+++ src/test/java/com/ecmp/apigateway/ApiAppServerTests.java
@@ -1,6 +1,6 @@
 package com.ecmp.apigateway;
 
-import com.ecmp.apigateway.service.IGatewayApiAppService;
+import com.ecmp.apigateway.service.IGatewayApiAppClient;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,22 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * @author:wangdayin
- * @date:2018/4/26
+ * @author: hejun
+ * @date: 2018/4/26
  * @remark:
  */
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiAppServerTests {
     @Autowired
-    private IGatewayApiAppService gatewayApiAppService;
+    private IGatewayApiAppClient gatewayApiAppClient;
 
     @Test
     @Ignore
     public void test() {
-        String result = this.gatewayApiAppService.findAllApiApp();
-        System.out.println("=======>" + result);
+        Object result = this.gatewayApiAppClient.findAllApiApp();
+        System.out.println("=======>" + result.toString());
     }
 
 }

5aee7ae765ee4aac2fe644ed4f85600472baaec8
贺俊 <jun5.he@changhong.com>
2018-04-26 23:28:34 +0800

    更新目录结构提交

---
+++ gateway-router-server/src/main/java/com/ecmp/apigateway/GatewayRouterServerApplication.java
@@ -0,0 +1,18 @@
+package com.ecmp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+
+@SpringBootApplication
+@EnableSpringConfigured
+@ServletComponentScan
+@EnableZuulProxy
+public class GatewayRouterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRouterServerApplication.class, args);
+    }
+}
---
+++ gateway-router-server/src/main/java/com/ecmp/apigateway/zuul/web/RouteController.java
@@ -0,0 +1,40 @@
+package com.ecmp.apigateway.zuul.web;
+
+import com.ecmp.apigateway.zuul.event.RefreshService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.web.ZuulHandlerMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * @author: hejun
+ * @date: 2018/4/24
+ * @remark: 路由配置-控制层跳转
+ */
+@Controller
+@RequestMapping(value = "/route_service")
+public class RouteController {
+    @Autowired
+    RefreshService refreshService;
+
+    @ResponseBody
+    @RequestMapping("refreshRoute")
+    public Object refreshRoute() {
+        refreshService.refreshRoute();
+        return "refreshRoute";
+    }
+
+    @Autowired
+    ZuulHandlerMapping zuulHandlerMapping;
+
+    @ResponseBody
+    @RequestMapping("watchNowRoute")
+    public Object watchNowRoute() {
+        //可以用debug模式看里面具体是什么
+        Map<String, Object> handlerMap = zuulHandlerMapping.getHandlerMap();
+        return handlerMap;
+    }
+}

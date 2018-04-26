fddd8d504142272551bece5b4e16d79a4fbaa741
贺俊 <jun5.he@changhong.com>
2018-04-26 15:12:07 +0800

    修改获取配置中心的接口

--- src/main/java/com/ecmp/apigateway/web/GatewayApiServiceController.java
+++ src/main/java/com/ecmp/apigateway/web/GatewayApiServiceController.java
@@ -4,7 +4,7 @@ import com.ecmp.apigateway.model.GatewayApiService;
 import com.ecmp.apigateway.model.PageModel;
 import com.ecmp.apigateway.model.ResponseModel;
 import com.ecmp.apigateway.model.SearchParam;
-import com.ecmp.apigateway.service.IGatewayApiAppService;
+import com.ecmp.apigateway.service.IGatewayApiAppClient;
 import com.ecmp.apigateway.service.IGatewayApiServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -27,7 +27,7 @@ public class GatewayApiServiceController {
     private IGatewayApiServiceService gatewayApiServiceService;
 
     @Autowired
-    private IGatewayApiAppService gatewayApiAppService;
+    private IGatewayApiAppClient gatewayApiAppClient;
 
     /**
      * 获取配置中心应用服务
@@ -36,7 +36,7 @@ public class GatewayApiServiceController {
     @ResponseBody
     @RequestMapping("findAllApiApp")
     public Object findAllApiApp() {
-        Object apiAppList = gatewayApiAppService.findAllApiApp();
+        Object apiAppList = gatewayApiAppClient.findAllApiApp();
         return ResponseModel.SUCCESS(apiAppList);
     }
 

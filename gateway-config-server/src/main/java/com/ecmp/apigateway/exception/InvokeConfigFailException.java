e50cd6890af80b0fdc0978c96621a830db0dcc4d
贺俊 <jun5.he@changhong.com>
2018-05-14 11:08:27 +0800

    修改调用服务的提示文字

--- gateway-config-server/src/main/java/com/ecmp/apigateway/exception/InvokeConfigFailException.java
+++ gateway-config-server/src/main/java/com/ecmp/apigateway/exception/InvokeConfigFailException.java
@@ -5,7 +5,7 @@ import com.ecmp.apigateway.exception.message.MessageRuntimeException;
 /**
  * @author: hejun
  * @date: 2018/4/26
- * @remark: 调用配置失败异常
+ * @remark: 调用配置服务失败异常
  */
 public class InvokeConfigFailException extends MessageRuntimeException {
     public InvokeConfigFailException() {
--- gateway-config-server/src/main/java/com/ecmp/apigateway/exception/InvokeRouteFailException.java
+++ gateway-config-server/src/main/java/com/ecmp/apigateway/exception/InvokeRouteFailException.java
@@ -5,7 +5,7 @@ import com.ecmp.apigateway.exception.message.MessageRuntimeException;
 /**
  * @author: hejun
  * @date: 2018/4/26
- * @remark: 调用路由失败异常
+ * @remark: 调用路由服务失败异常
  */
 public class InvokeRouteFailException extends MessageRuntimeException {
     public InvokeRouteFailException() {

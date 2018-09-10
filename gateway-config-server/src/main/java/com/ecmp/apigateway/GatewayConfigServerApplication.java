f9f9e3d93ab7fb43f112bdd61cf915b03ef106b8
paul <lslin6@gmail.com>
2018-09-10 11:07:36 +0800

    Token验证

--- gateway-config-server/src/main/java/com/ecmp/apigateway/GatewayConfigServerApplication.java
+++ gateway-config-server/src/main/java/com/ecmp/apigateway/GatewayConfigServerApplication.java
@@ -1,9 +1,11 @@
 package com.ecmp.apigateway;
 
+import com.ecmp.util.JwtTokenUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,5 +17,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GatewayConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayConfigServerApplication.class, args);
+    }
+
+
+    @Bean
+    public JwtTokenUtil jwtTokenUtil() {
+        JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
+        //TODO 设置超时时间
+        jwtTokenUtil.setJwtExpiration(3600 * 10);
+        return jwtTokenUtil;
     }
 }

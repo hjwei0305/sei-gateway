2d6a9b2a9d921cc954e277a7232ce73e147e04ae
liusonglin <songlin.liu@changhong.com>
2020-01-16 13:25:21 +0800

    内部令牌处理

--- src/main/java/com/ecmp/apigateway/intergration/AuthFromAccountCenter.java
+++ src/main/java/com/ecmp/apigateway/intergration/AuthFromAccountCenter.java
@@ -5,15 +5,16 @@ import com.ecmp.apigateway.entity.ResultData;
 import com.ecmp.apigateway.intergration.fallback.AuthFromAccountCenterFallBackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(value = "authFormAccountCenter", url = "${auth.host}", path = "${auth.path}", fallbackFactory = AuthFromAccountCenterFallBackFactory.class)
 public interface AuthFromAccountCenter {
 
-    @GetMapping(value = "/auth/getAnonymousToken",consumes = "application/json")
+    @PostMapping(value = "/auth/getAnonymousToken",consumes = "application/json")
     ResultData<String> getAnonymousToken();
 
 
-    @GetMapping(value = "/auth/check",consumes = "application/json")
+    @PostMapping(value = "/auth/check",consumes = "application/json")
     ResultData<String> check(String sid);
 
 }

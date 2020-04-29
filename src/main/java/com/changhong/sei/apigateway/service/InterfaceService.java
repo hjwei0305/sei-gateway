2a18c69a12e5efc0f50978b1107f216908cc7f6b
Vision.Mac <chao2.ma@changhong.com>
2020-04-29 20:37:34 +0800

    网关增加cookie

--- src/main/java/com/changhong/sei/apigateway/service/InterfaceService.java
+++ src/main/java/com/changhong/sei/apigateway/service/InterfaceService.java
@@ -2,10 +2,10 @@ package com.changhong.sei.apigateway.service;
 
 import com.changhong.sei.apigateway.dao.GatewayInterfaceDao;
 import com.changhong.sei.apigateway.entity.GatewayInterface;
-import com.changhong.sei.core.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -28,12 +28,11 @@ public class InterfaceService {
     @Autowired
     private GatewayInterfaceDao gatewayInterfaceDao;
     @Autowired
-//    private RedisTemplate<String,String> redisTemplate;
-    private CacheBuilder cacheBuilder;
+    private RedisTemplate<String,String> redisTemplate;
 
 
     public Boolean checkToken(String uri) {
-        return Objects.isNull(cacheBuilder.get(key(uri)));
+        return Objects.isNull(redisTemplate.opsForValue().get(key(uri)));
     }
 
     /**
@@ -46,14 +45,14 @@ public class InterfaceService {
             return;
         }
         interfaceList.forEach(gi -> {
-            if (!gi.getValidateToken() && !gi.isDeleted()) {
-                cacheBuilder.set(key(gi.getInterfaceURI()), "0");
+            if(!gi.getValidateToken() && !gi.isDeleted()){
+                redisTemplate.opsForValue().set(key(gi.getInterfaceURI()), "0");
             }
         });
     }
 
     private String key(String uri) {
-        String keyTemplate = uri.startsWith("/") ? uri : "/" + uri;
+        String keyTemplate = uri.startsWith("/")?uri:"/"+ uri;
         return "Gateway:NoToken" + keyTemplate.replaceAll("/", ":");
     }
 
@@ -63,27 +62,27 @@ public class InterfaceService {
 
     public GatewayInterface save(GatewayInterface gi) {
         gi = gatewayInterfaceDao.save(gi);
-        if (!gi.getValidateToken()) {
-            cacheBuilder.set(key(gi.getInterfaceURI()), "0");
-        } else {
-            cacheBuilder.remove(key(gi.getInterfaceURI()));
+        if(!gi.getValidateToken()){
+            redisTemplate.opsForValue().set(key(gi.getInterfaceURI()), "0");
+        }else {
+            redisTemplate.delete(key(gi.getInterfaceURI()));
         }
         return gi;
     }
 
     public void delete(String id) {
         Optional<GatewayInterface> gi = gatewayInterfaceDao.findById(id);
-        if (gi.isPresent()) {
+        if(gi.isPresent()){
             gi.get().setDeleted(true);
             gatewayInterfaceDao.save(gi.get());
-            cacheBuilder.remove(key(gi.get().getInterfaceURI()));
+            redisTemplate.delete(key(gi.get().getInterfaceURI()));
         }
     }
 
     public Boolean reloadCache() {
-        Set<String> gatewayKeys = cacheBuilder.keys("Gateway:NoToken*");
-        if (!CollectionUtils.isEmpty(gatewayKeys)) {
-            gatewayKeys.forEach(key -> cacheBuilder.remove(key));
+        Set<String> gatewayKeys = redisTemplate.keys("Gateway:NoToken*");
+        if(!CollectionUtils.isEmpty(gatewayKeys)){
+            redisTemplate.delete(gatewayKeys);
         }
 
         this.loadRuntimeData();

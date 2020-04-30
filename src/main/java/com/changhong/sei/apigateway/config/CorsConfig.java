212158493275e988004c39d0972a117db44d11fb
Vision.Mac <chao2.ma@changhong.com>
2020-04-30 17:36:14 +0800

    尝试解决跨域问题

--- src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
+++ src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
@@ -23,30 +23,34 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class CorsConfig {
 
-//    private static final String ALLOWED_HEADERS = "*";
-//    private static final String ALLOWED_METHODS = "*";
-//    private static final String ALLOWED_ORIGIN = "*";
-//    private static final String MAX_AGE = "18000L";
-//
-//    @Bean
-//    public WebFilter corsFilter() {
-//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-//            ServerHttpRequest request = ctx.getRequest();
-//            if (CorsUtils.isCorsRequest(request)) {
-//                ServerHttpResponse response = ctx.getResponse();
-//                HttpHeaders headers = response.getHeaders();
-//                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-//                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-//                headers.add("Access-Control-Max-Age", MAX_AGE);
-//                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-//                if (request.getMethod() == HttpMethod.OPTIONS) {
-//                    response.setStatusCode(HttpStatus.OK);
-//                    return Mono.empty();
-//                }
-//            }
-//            return chain.filter(ctx);
-//        };
-//    }
+    //这里为支持的请求头，如果有自定义的header字段请自己添加（不知道为什么不能使用*）
+    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,X-Authorization,x-sid,Content-Disposition";
+    private static final String ALLOWED_METHODS = "*";
+    private static final String ALLOWED_ORIGIN = "*";
+    private static final String ALLOWED_Expose = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,X-Authorization,x-sid,Content-Disposition";
+    private static final String MAX_AGE = "18000L";
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+                headers.add("Access-Control-Max-Age", MAX_AGE);
+                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+                headers.add("Access-Control-Expose-Headers", ALLOWED_Expose);
+                headers.add("Access-Control-Allow-Credentials", "true");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
 
     @Bean
     public WebFilter contextPathWebFilter(Environment env) {

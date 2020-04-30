1d93e8855d2e64802cbced8ee9bd069932dff258
Vision.Mac <chao2.ma@changhong.com>
2020-04-30 17:25:25 +0800

    舱室解决跨域问题

--- src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
+++ src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
@@ -23,30 +23,30 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class CorsConfig {
 
-    private static final String ALLOWED_HEADERS = "*";
-    private static final String ALLOWED_METHODS = "*";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String MAX_AGE = "18000L";
-
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
-                ServerHttpResponse response = ctx.getResponse();
-                HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-        };
-    }
+//    private static final String ALLOWED_HEADERS = "*";
+//    private static final String ALLOWED_METHODS = "*";
+//    private static final String ALLOWED_ORIGIN = "*";
+//    private static final String MAX_AGE = "18000L";
+//
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isCorsRequest(request)) {
+//                ServerHttpResponse response = ctx.getResponse();
+//                HttpHeaders headers = response.getHeaders();
+//                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+//                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+//                headers.add("Access-Control-Max-Age", MAX_AGE);
+//                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+//                if (request.getMethod() == HttpMethod.OPTIONS) {
+//                    response.setStatusCode(HttpStatus.OK);
+//                    return Mono.empty();
+//                }
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
 
     @Bean
     public WebFilter contextPathWebFilter(Environment env) {

b157a3d96c74c38ea4d6ffe13a3accd834553917
Vision.Mac <chao2.ma@changhong.com>
2020-04-30 18:10:21 +0800

    网关增加cookie

--- src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
+++ src/main/java/com/changhong/sei/apigateway/config/CorsConfig.java
@@ -23,13 +23,6 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class CorsConfig {
 
-    //这里为支持的请求头，如果有自定义的header字段请自己添加（不知道为什么不能使用*）
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, nevercancel, credential, X-XSRF-TOKEN,X-Authorization,x-sid,Content-Disposition";
-    private static final String ALLOWED_METHODS = "*";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String ALLOWED_Expose = ALLOWED_HEADERS;
-    private static final String MAX_AGE = "18000L";
-
     @Bean
     public WebFilter corsFilter() {
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
@@ -37,11 +30,11 @@ public class CorsConfig {
             if (CorsUtils.isCorsRequest(request)) {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-                headers.add("Access-Control-Expose-Headers", ALLOWED_Expose);
+                headers.add("Access-Control-Allow-Origin", "*");
+                headers.add("Access-Control-Allow-Methods", "*");
+                headers.add("Access-Control-Max-Age", "18000L");
+                headers.add("Access-Control-Allow-Headers", "*");
+                headers.add("Access-Control-Expose-Headers", "*");
                 headers.add("Access-Control-Allow-Credentials", "true");
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);

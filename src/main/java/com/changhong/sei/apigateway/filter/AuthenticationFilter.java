1b1d84ad0cf2a3b0f1a915ec87345bfe5ffbf4a3
Vision.Mac <chao2.ma@changhong.com>
2020-05-14 20:29:58 +0800

    edm预览组件忽略token检查

--- src/main/java/com/changhong/sei/apigateway/filter/AuthenticationFilter.java
+++ src/main/java/com/changhong/sei/apigateway/filter/AuthenticationFilter.java
@@ -104,7 +104,7 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     }
 
     private boolean shouldFilter(String uri) {
-        if (StringUtils.containsIgnoreCase(uri, "/sso/") || StringUtils.containsIgnoreCase(uri, "/monitor/health")) {
+        if (StringUtils.containsAny(uri, "/sso/", "/monitor/health", "/edm-service/pdfjs/")) {
             return false;
         } else {
             String contextPath = env.getProperty("server.servlet.context-path", "/api-gateway");

e4d789b4b2269e1b77c34d2448f9dc8b19ee144c
Vision.Mac <chao2.ma@changhong.com>
2021-01-07 09:41:40 +0800

    升级springboot到2.3.7

--- src/main/java/com/changhong/sei/apigateway/controller/RoutesController.java
+++ src/main/java/com/changhong/sei/apigateway/controller/RoutesController.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.gateway.actuate.GatewayControllerEndpoint;
 import org.springframework.cloud.zookeeper.discovery.ZookeeperDiscoveryProperties;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
@@ -30,7 +31,7 @@ public class RoutesController {
     private CuratorFramework curatorFramework;
 
     @GetMapping("getAllRoutes")
-    public Flux<Map<String, Object>> getAllRoutes(){
+    public Mono<List<Map<String, Object>>> getAllRoutes(){
         return gatewayControllerEndpoint.routes();
     }
 

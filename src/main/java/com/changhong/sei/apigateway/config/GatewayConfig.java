12200a8b7b2a9efe60b12cab63998601da116198
Vision.Mac <chao2.ma@changhong.com>
2021-03-31 16:23:39 +0800

    解决跨域问题1

--- src/main/java/com/changhong/sei/apigateway/config/GatewayConfig.java
+++ src/main/java/com/changhong/sei/apigateway/config/GatewayConfig.java
@@ -52,30 +52,30 @@ public class GatewayConfig {
             return chain.filter(exchange);
         };
     }
-
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
-//                HttpHeaders requestHeaders = request.getHeaders();
-                ServerHttpResponse response = ctx.getResponse();
-//                HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
-                HttpHeaders headers = response.getHeaders();
-                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,"*");
-                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*");
-                headers.set(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
-                headers.set(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-                    response.setStatusCode(HttpStatus.OK);
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-        };
-    }
+//
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isCorsRequest(request)) {
+////                HttpHeaders requestHeaders = request.getHeaders();
+//                ServerHttpResponse response = ctx.getResponse();
+////                HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+//                HttpHeaders headers = response.getHeaders();
+//                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+//                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,"*");
+//                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+//                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*");
+//                headers.set(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+//                headers.set(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+//                if (request.getMethod() == HttpMethod.OPTIONS) {
+//                    response.setStatusCode(HttpStatus.OK);
+//                    return Mono.empty();
+//                }
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
 
     @Bean
     public ServerCodecConfigurer serverCodecConfigurer() {

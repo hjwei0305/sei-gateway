9aadd6e4cf664d7e84999381f01e730d1faf27fd
Vision.Mac <chao2.ma@changhong.com>
2021-06-23 14:03:28 +0800

    ip获取测试

--- src/main/java/com/changhong/sei/apigateway/filter/AccessLogGlobalFilter.java
+++ src/main/java/com/changhong/sei/apigateway/filter/AccessLogGlobalFilter.java
@@ -30,7 +30,7 @@ import java.util.Objects;
  * @author 马超(Vision.Mac)
  * @version 1.0.00  2021-01-12 13:33
  */
-//@Component
+@Component
 public class AccessLogGlobalFilter implements GlobalFilter, Ordered {
     private static final Logger LOG = LoggerFactory.getLogger(AccessLogGlobalFilter.class);
 
